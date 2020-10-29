e1fcd62e61e1c39b669f194a3baabc47fe7e6a73
Eojin K1m <eojin.dev@gmail.com>
2020-10-29 11:50:16 +0900

    Feat create AuthController and define signIN

--- src/main/java/controllers/AuthController.java
+++ src/main/java/controllers/AuthController.java
@@ -1,2 +1,18 @@
-package controllers;public class AuthController {
+package controllers;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/auth")
+@RestController
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.OK)
+    public SignInResponse signIn(){}
 }
