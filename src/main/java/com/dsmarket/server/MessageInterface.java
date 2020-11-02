5484cc431a2226601cbbeb76436aad03e3f9d8f0
6B6390AA <49777427+6B6390AA@users.noreply.github.com>
2020-11-02 20:32:10 +0900

    [UPDATE] sending e-mails

---
+++ src/main/java/com/dsmarket/server/MessageInterface.java
@@ -0,0 +1,6 @@
+package com.dsmarket.server;
+
+public interface MessageInterface 
+{
+	public String sendMessage(String dest)throws Exception;
+}
---
+++ src/main/java/com/dsmarket/server/SendEmail.java
@@ -0,0 +1,65 @@
+package com.dsmarket.server;
+
+import java.util.Random;
+
+import javax.mail.Message.RecipientType;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SendEmail implements MessageInterface{
+	
+	@Autowired
+	JavaMailSender emailSender;
+	
+	private String key = createKey();
+	
+	 private MimeMessage createMessage(String dest) throws Exception
+	 {
+
+		 MimeMessage message = emailSender.createMimeMessage();
+		 	
+		 	message.addRecipients(RecipientType.TO, dest);
+	        message.setSubject("대마켓 이메일 인증 테스트");
+	        
+	        String msg= 	
+	        		"<h1> 대마켓 이메일 인증 테스트입니다.</h1><br>"
+	       		+	"<p>인증 코드는<p><br>"
+	       		+	"<strong>"+key+"</strong><br> "
+				+	"<p>입니다.<p><br>";
+	       	
+	        message.setText(msg, "utf-8", "html");
+	        message.setFrom(new InternetAddress("kku3472@gmail.com","DaeMarket"));
+	        
+	        return message;
+	    }
+
+		public static String createKey() 
+		{
+			Random rnd = new Random();
+			int code = rnd.nextInt(89999999) + 10000000;
+			return Integer.toString(code);
+		}
+
+	
+	@Override
+	public String sendMessage(String dest) throws Exception 
+	{
+		MimeMessage message = createMessage(dest);
+		        try 
+		        {
+		        	emailSender.send(message);
+		        	System.out.println("Email sent!");
+		        }
+		        catch(MailException ex) 
+		        {
+		            throw new IllegalArgumentException();
+		        }
+		        return key;
+	}
+}
