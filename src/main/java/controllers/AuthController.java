e224550656f59d827405795b02d7d99371ddae83
Eojin K1m <eojin.dev@gmail.com>
2020-11-04 23:32:41 +0900

    Modify Auth package name 2 auth

--- src/main/java/controllers/AuthController.java
+++ src/main/java/controllers/AuthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import services.Auth.AuthServiceImpl;
+import services.auth.AuthServiceImpl;
 
 @RequestMapping("/auth")
 @RestController
