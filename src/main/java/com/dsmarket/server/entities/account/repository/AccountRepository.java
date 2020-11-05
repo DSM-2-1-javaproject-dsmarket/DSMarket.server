951f516954f8085798add2708d9b03fe392387f2
Eojin K1m <eojin.dev@gmail.com>
2020-11-06 08:40:09 +0900

    Add findById, save func 2 AccountRepository

--- src/main/java/com/dsmarket/server/entities/account/repository/AccountRepository.java
+++ src/main/java/com/dsmarket/server/entities/account/repository/AccountRepository.java
@@ -4,7 +4,10 @@ import com.dsmarket.server.entities.account.Account;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AccountRepository extends CrudRepository<Account, String> {
-
+    Optional<Account> findById(String id);
+    Account save(Account account);
 }
