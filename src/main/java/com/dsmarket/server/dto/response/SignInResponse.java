e261572a8a334a28c01cff35648d13bd2075b214
Eojin K1m <eojin.dev@gmail.com>
2020-11-06 08:42:44 +0900

    Modify sign in response fields

--- src/main/java/com/dsmarket/server/dto/response/SignInResponse.java
+++ src/main/java/com/dsmarket/server/dto/response/SignInResponse.java
@@ -2,13 +2,13 @@ package com.dsmarket.server.dto.response;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 @AllArgsConstructor
 public class SignInResponse {
 
-    private String id;
-
-    private String password;
+    private String accessToken;
 }
