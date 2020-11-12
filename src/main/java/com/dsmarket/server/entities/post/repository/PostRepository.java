8f510012bc9a8a5785b5e9aa7048117d18cc58f4
Eojin K1m <eojin.dev@gmail.com>
2020-11-13 08:54:43 +0900

    Add Page findAll

--- src/main/java/com/dsmarket/server/entities/post/repository/PostRepository.java
+++ src/main/java/com/dsmarket/server/entities/post/repository/PostRepository.java
@@ -1,8 +1,12 @@
 package com.dsmarket.server.entities.post.repository;
 
 import com.dsmarket.server.entities.post.Post;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends CrudRepository<Post, Integer> {
+public interface PostRepository extends JpaRepository<Post, Integer> {
     Post save(Post post);
+
+    Page<Post> findAll(Pageable pageable);
 }
