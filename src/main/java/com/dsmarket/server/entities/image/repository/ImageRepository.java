ab605d4b003138e54c0b5903ecab34695c1b8f29
eojin <eojin.dev@gmail.com>
2020-12-01 15:11:27 +0900

    Create image entity, repo, S3Service

--- src/main/java/com/dsmarket/server/entities/image/repository/ImageRepository.java
+++ src/main/java/com/dsmarket/server/entities/image/repository/ImageRepository.java
@@ -3,5 +3,5 @@ package com.dsmarket.server.entities.image.repository;
 import com.dsmarket.server.entities.image.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ImageRepository extends JpaRepository<Image, Integer> {
+public interface ImageRepository extends JpaRepository<Image, String> {
 }
